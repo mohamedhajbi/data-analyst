8d8273f32d5ae311385bc9e44e1f6aebd552f063
Mohamed Hajbi <hajbi662@gmail.com>
2024-06-14 17:58:25 +0000

    add "Percentage of appearance as a function of views category"

--- analyst/analist.py
+++ analyst/analist.py
@@ -5,6 +5,7 @@ from googleapiclient.http import MediaIoBaseDownload
 import io
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 
 SERVICE_ACCOUNT_FILE = 'service.json'
 
@@ -26,8 +27,6 @@ def download_csv(file_id):
     df = pd.read_csv(fh)
 
     columns_to_drop = ['tags', 'thumbnail_link', 'comments_disabled', 'ratings_disabled', 'description']
-
-    # Drop only the columns that exist in the DataFrame
     df.drop(columns=[col for col in columns_to_drop if col in df.columns], axis=1, inplace=True)
 
     # Further processing
@@ -36,8 +35,25 @@ def download_csv(file_id):
     df['published_hour'] = df['publishedAt'].dt.hour
     df['published_day'] = df['publishedAt'].dt.strftime('%A')
     df['ratio'] = df['likes'] / (df['likes'] + df['dislikes'])
+    df['views_cat'] = df.views.apply(views_cat)
 
     return df
+
+def views_cat(views):
+    if views <= 10000: return 'less than 10k'
+    if views <= 25000: return '25k'
+    if views <= 50000: return '50k'
+    if views <= 75000: return '75k'
+    if views <= 100000: return '100k'
+    if views <= 250000: return '250k'
+    if views <= 500000: return '500k'
+    if views <= 750000: return '750k'
+    if views <= 1000000: return '1m'
+    if views <= 2500000: return '2.5m'
+    if views <= 5000000: return '5m'
+    if views <= 7500000: return '7.5m'
+    if views <= 10000000: return '10m'
+    return '15m' if views <= 15000000 else 'more than 15m'
 
 def top_20(df):
     grouped = pd.pivot_table(df, index='channel_title', columns='published_year', values='video_id', aggfunc='count', margins=True, margins_name='All')
@@ -61,7 +77,6 @@ def top_20(df):
         2017: '#1f77b4',
         2018: '#ff7f0e'
     }
-
     for year in grouped.columns:
         if year != 'All':
             figure.add_trace(go.Bar(
@@ -69,9 +84,10 @@ def top_20(df):
                 x=grouped[year],
                 name=str(year),
                 orientation='h',
-                marker_color=colors.get(year, '#000000') 
+                marker_color=colors.get(year, '#000000')  
             ))
 
+    # Customize layout
     figure.update_layout(
         barmode='stack',
         title='The 20 Top performing channels ordered by their appearance on the trending page',
@@ -89,3 +105,16 @@ def top_20(df):
     )
 
     return figure
+
+def pie_chart(df):
+    df['views_cat'] = df.views.apply(views_cat)
+    appreanf_f_view = df.groupby('views_cat').size().reset_index(name='counts').sort_values(by='counts', ascending=True)
+    sorter = ['less than 10k', '25k', '50k', '75k', '100k', '250k', '500k', '750k', '1m', '2.5m', '5m', '7.5m', '10m', '15m', 'more than 15m']
+    sorterIndex = dict(zip(sorter, range(len(sorter))))
+    appreanf_f_view['rank'] = appreanf_f_view['views_cat'].map(sorterIndex)
+    appreanf_f_view = appreanf_f_view.sort_values(by='rank')
+    appreanf_f_view.drop(['rank'], axis=1, inplace=True)
+
+    fig = px.pie(appreanf_f_view, values='counts', names='views_cat', title='Percentage of appearance as a function of views category')
+    
+    return fig
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
-from analyst.analist import download_csv, top_20 
+from analyst.analist import download_csv, top_20 , pie_chart
 import os
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -12,6 +12,8 @@ FILE_ID = '1K-dP_S7se5K6h7o6e6ocKdBtetCU2Fyc'
 
 app.layout = html.Div([
     html.Div(id='graph-container'),
+    html.Div(id='pie-container'),
+
     dcc.Interval(
         id='interval-component',
         interval=60 * 1000,  
@@ -21,12 +23,14 @@ app.layout = html.Div([
 
 @app.callback(
     Output('graph-container', 'children'),
+    Output('pie-container', 'children'),
     [Input('interval-component', 'n_intervals')]
 )
 def update_graph(n_intervals):
     df = download_csv(FILE_ID)
     figure = top_20(df)
-    return dcc.Graph(figure=figure)
+    fig = pie_chart(df)
+    return dcc.Graph(figure=figure), dcc.Graph(figure=fig)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
